5c8a87f22c8d296630898dca5067c0c547495b3b
J-Andersen-UvA <j.andersen@uva.nl>
2025-02-25 11:52:24 +0100

    added a function to editorFuncs.py in order to open an animation using a window

--- version2/scripts/editorFuncs.py
+++ version2/scripts/editorFuncs.py
@@ -12,3 +12,20 @@ def get_actor_by_name(name):
         if name in actor.get_name():
             return actor
     return None
+
+def open_anim_in_window(path):
+    """
+    Open an animation asset in a new window.
+
+    Params:
+    - path (str): The path to the animation asset.
+    """
+    animation_asset = unreal.EditorAssetLibrary.load_asset(path)
+    if animation_asset:
+        unreal.AssetToolsHelpers.get_asset_tools().open_editor_for_assets([animation_asset])
+        editor_window = unreal.SystemLibrary.get_editor_main_window()
+        if editor_window:
+            editor_window.resize(1920, 1080)
+            editor_window.set_focus()
+    else:
+        print(f"Failed to load animation asset: {path}")
