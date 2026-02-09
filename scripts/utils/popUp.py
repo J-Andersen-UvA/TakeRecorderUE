a5db195233a81cabb844483b5de2ea697b28a443
Jari <j.andersen@uva.nl>
2026-02-09 12:39:51 +0100

    support for timed popUps added

--- scripts/utils/popUp.py
+++ scripts/utils/popUp.py
@@ -1,4 +1,5 @@
 import unreal
+import time
 
 def show_popup_message(title, message):
     """
@@ -14,5 +15,15 @@ def show_popup_message(title, message):
         message_type=unreal.AppMsgType.OK
     )
 
+def show_timed_popup(title, message, seconds=5):
+    with unreal.ScopedSlowTask(seconds, title) as task:
+        task.make_dialog(True)  # True = allow cancel
+        for i in range(seconds):
+            if task.should_cancel():
+                break
+            task.enter_progress_frame(1, f"{message} ({seconds - i}s)")
+            time.sleep(1)
+
 # # Example usage
 # show_popup_message("Information", "This is a pop-up message from Unreal Python!")
+# show_timed_popup("Please Re-record!", "Neem opnieuw op!", seconds=5)
